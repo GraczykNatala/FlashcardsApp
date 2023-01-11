0726c67b4d61447fbe4b4b9e1e82e6fe90b66caa
Natalia Graczyk <natka.graczyk@gmail.com>
2023-01-11 16:10:07 +0100

    create class Flashcard with method createFlashcard

---
+++ FlashcardsApp/Flashcard.cs
@@ -0,0 +1,31 @@
+namespace FlashcardsApp;
+
+ class Flashcard
+{
+    public string Text { get; set; }
+    public string Meaning { get; set; }
+    public Flashcard()
+    {
+    }
+    public Flashcard(string text, string meaning)
+    {
+        this.Text = text;
+        this.Meaning = meaning;
+    }
+
+    public override string ToString()
+    {
+        return Text + "-" + Meaning;
+    }
+
+    Flashcard createFlashcard()
+    {
+        Console.WriteLine("Podaj słowo");
+        string text = Console.ReadLine();
+        Console.WriteLine("Podaj znaczenie");
+        string meaning = Console.ReadLine();
+        Flashcard fc = new Flashcard(text, meaning);
+        return fc;
+    }
+    
+}
